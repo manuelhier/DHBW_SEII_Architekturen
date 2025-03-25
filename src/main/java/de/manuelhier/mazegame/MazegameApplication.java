9602844750bc98445b4c768ce94ed48f877c9d8f
Manuel Rettig <manuel.rettig@mail.de>
2025-03-25 16:25:30 +0100

    Add maven project with spring web based on spring initializr

---
+++ src/main/java/de/manuelhier/mazegame/MazegameApplication.java
@@ -0,0 +1,13 @@
+package de.manuelhier.mazegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazegameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazegameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/manuelhier/mazegame/MazegameApplicationTests.java
@@ -0,0 +1,13 @@
+package de.manuelhier.mazegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazegameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
